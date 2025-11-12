1f9df2cbab6b4c8e82b341cb4673dc0c85860b6a
Mohamed <modelbashir@gmail.com>
2025-11-12 18:26:22 -0500

    Initial Commit

---
+++ NeuroMetrica/NeuroMetricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuroMetricaApp.swift
+//  NeuroMetrica
+//
+//  Created by Mohamed Elbashir on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NeuroMetricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
